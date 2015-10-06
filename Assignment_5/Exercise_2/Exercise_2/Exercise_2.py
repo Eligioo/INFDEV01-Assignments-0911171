3f0575f45d01195a61e7c861dc3337f43cd04603
Stefan Koolen <nast3zz@gmail.com>
2015-10-06 22:49:38 +0200

    Minor variable changes

--- Assignment_5/Exercise_2/Exercise_2/Exercise_2.py
+++ Assignment_5/Exercise_2/Exercise_2/Exercise_2.py
@@ -1,11 +1,11 @@
 import re
 
-sValue = raw_input("Choose a password!")
-if re.match("(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[&'(\-_)=~#{[|`\\^@\]}^$*иг╡%,;:!?./з+]).{6,50}", sValue):
-    sMessage = "Password is strong!"         
-elif re.match('(?=.*[a-z])(?=.*[0-9]).{6,50}', sValue):
-    sMessage = "Password is medium"
+password = raw_input("Choose a password!")
+if re.match("(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[&'(\-_)=~#{[|`\\^@\]}^$*иг╡%,;:!?./з+]).{6,50}", password):
+    strength = "Password is strong!"         
+elif re.match('(?=.*[a-z])(?=.*[0-9]).{6,50}', password):
+    strength = "Password is medium"
 else:
-    sMessage = "Password is weak!"
+    strength = "Password is weak!"
 
-print sMessage
+print strength
