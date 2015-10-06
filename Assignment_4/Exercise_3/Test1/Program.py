30cedde44e8578dd4c5db4b751d1082a9fe78a6b
Stefan Koolen <nast3zz@gmail.com>
2015-10-06 08:44:51 +0200

    Hand in assignment 4 exercise 3
    Turtle

---
+++ Assignment_4/Exercise_3/Test1/Program.py
@@ -0,0 +1,33 @@
+﻿from Start import *
+print "For the instructions press 'i'\n"
+
+
+def Program():
+    key = get()
+
+    if key == 119:
+        forward(5)
+    elif key == 97:
+        turn(-20)
+    elif key == 100:
+        turn(20)
+    elif key == 115:
+        forward(-5)
+    elif key == 49:
+        change_color_to("Black")
+    elif key == 50:
+        change_color_to("Red")
+    elif key == 51:
+        change_color_to("Blue")
+    elif key == 52:
+        change_color_to("Green")
+    elif key == 105:
+        instructions("W,A,S and D for movement.\n1,2,3 and 4 to change color.\nPress 'c' to clear all the lines")
+    elif key == 99:
+        clear()
+
+    print key
+
+
+run(Program)
+from End import *
