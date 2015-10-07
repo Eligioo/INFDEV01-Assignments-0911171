7ce577b15b834cfed48b24243e5bc42683353149
Stefan Koolen <nast3zz@gmail.com>
2015-10-07 21:05:02 +0200

    Update password generator

--- Assignment_5/Exercise_2/Exercise_2/Exercise_2.py
+++ Assignment_5/Exercise_2/Exercise_2/Exercise_2.py
@@ -1,11 +1,35 @@
-﻿import re
+﻿password = raw_input("Choose a password \n")
+password_length = len(password)
+result = ""
 
-password = raw_input("Choose a password!")
-if re.match("(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[&'(\-_)=~#{[|`\\^@\]}^$*¨£µ%,;:!?./§+]).{6,50}", password):
-    strength = "Password is strong!"         
-elif re.match('(?=.*[a-z])(?=.*[0-9]).{6,50}', password):
-    strength = "Password is medium"
+upperChars      = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowerChars      = "abcdefghijklmnopqrstuvwxyz"
+digitChars      = "0123456789"
+specialChars    = "!@#$%^&*()-_=+[]{};:'<>,./?"
+
+lowerCount = 0
+upperCount = 0
+digitCount = 0
+specialCount = 0
+
+for char in password: 
+    if char in upperChars:
+        upperCount = upperCount + 1
+    elif char in digitChars:
+        digitCount = digitCount + 1
+    elif char in specialChars:
+        specialCount = specialCount + 1
+    elif char in lowerChars:
+        lowerCount = lowerCount + 1
+
+if specialCount > 0 and digitCount > 1 and upperCount > 1 and lowerCount > 1:
+    result = "Password is strong"
+elif digitCount > 1 and upperCount > 1 and lowerCount > 1:
+    result = "Password is medium"
+elif password_length < 6:
+    result = "Password is to short."
 else:
-    strength = "Password is weak!"
+    result = "Password is weak"
 
-print strength
+print result
+
