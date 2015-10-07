e628f464ea7a174da388d40a19081d2b56c62e92
Stefan Koolen <nast3zz@gmail.com>
2015-10-07 12:13:27 +0200

    Hand in Assingment 6 iososceles triangle

--- Assignment_6/isosceles_triangle.py
+++ Assignment_6/isosceles_triangle.py
@@ -2,7 +2,10 @@ height = input("Enter an integer for the height: ")
 result = ""
 
 for x in range(0, height):
-	for y in range(0, height):
+	generate = (x * 2) + 1
+	for y in range(1, height - x):
+		result += " "
+	for z in range(0, generate):
 		result += "*"
 	result += "\n"
 print result
