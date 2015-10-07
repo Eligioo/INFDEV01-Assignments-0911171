eca4263bbaad04609ecda8b81a4adcbc7d370114
Stefan Koolen <nast3zz@gmail.com>
2015-10-07 18:34:06 +0200

    Full circle final version

--- Assignment_6/full_circle/full_circle/full_circle.py
+++ Assignment_6/full_circle/full_circle/full_circle.py
@@ -1,13 +1,15 @@
 ﻿import math
+#afstand van de huidige x en y tot het middelpunt is de berekenen met de formule, wanneer de afstand kleiner is dan de radius moet je een asterix printen
 user_input = input("Give a size for the circle: (positive integer) ")
 radius = user_input/2
 result = ""
 for x in range(user_input):
     for y in range(user_input):
-        DistX = x - radius
-        DistY = y - radius
-        Dist = math.sqrt((DistY**2) + (DistX**2))
-        if Dist < radius:
+        distX = (x - radius) * (x - radius)
+        distY = (y - radius) * (y - radius)
+        distance = math.sqrt(distX + distY)
+        if distance < radius:
+            #print("("+str(distX)+", "+str(distY)+"), "+str(loc))
             result += "*"
         else:
             result += " "
