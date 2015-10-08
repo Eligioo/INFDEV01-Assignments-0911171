24c4b70f2a9d47b7c95d293a4db11676572715c5
Stefan Koolen <nast3zz@gmail.com>
2015-10-08 21:22:05 +0200

    Changes to isosceles

--- Assignment_6/isosceles_triangle.py
+++ Assignment_6/isosceles_triangle.py
@@ -2,7 +2,9 @@ height = input("Enter an integer for the height: ")
 result = ""
 
 for x in range(0, height):
+	#formula to calculate amount of stars that must be generated
 	generate = (x * 2) + 1
+	print generate
 	for y in range(1, height - x):
 		result += " "
 	for z in range(0, generate):
